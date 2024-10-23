bc28034c0da5cb3fb7766f5bb383a105ee631032
Inaaaa123 <zryzeng@163.com>
2024-10-23 21:22:28 +0800

    Extract CodeSandbox from backend codes, and use template method to improve sandbox' flexibility.

---
+++ src/main/java/com/letcode/codesandbox/CodeSandbox.java
@@ -0,0 +1,19 @@
+package com.letcode.codesandbox;
+
+
+import com.letcode.codesandbox.model.ExecuteCodeRequest;
+import com.letcode.codesandbox.model.ExecuteCodeResponse;
+
+/**
+ * 代码沙箱接口定义
+ */
+public interface CodeSandbox {
+
+    /**
+     * 执行代码
+     *
+     * @param executeCodeRequest
+     * @return
+     */
+    ExecuteCodeResponse executeCode(ExecuteCodeRequest executeCodeRequest);
+}
